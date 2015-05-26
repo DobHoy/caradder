8d14aba4d0f60c85fc31e369e9de2dfd0f3131df
Sri Mohan <dob.hoy@gmail.com>
2015-05-26 15:49:54 +0100

    first commit

--- TableViewAdder/ViewController.m
+++ TableViewAdder/ViewController.m
@@ -9,10 +9,43 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    BOOL _isInsertingNewCar;
+
+}
+
+@property (nonatomic, strong) UIButton *insertCar;
+@property (nonatomic, strong) UIButton *editCar;
+@property (nonatomic, strong) UIView *footerView;
+@property (nonatomic, strong) NSArray *favoriteCars;
 
 @end
 
 @implementation ViewController
+
+
+
+
+- (instancetype) init {
+    self = [super init];
+    
+    if(self) {
+        
+        self.favoriteCars = @[@"Lexus", @"Toyata", @"Mercedes", @"BMW", @"Audi"];
+        self.insertCar = [UIButton buttonWithType:UIButtonTypeContactAdd];
+        self.editCar = [UIButton buttonWithType:UIButtonTypeInfoDark];
+        
+        self.footerView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, 40)];
+        [self.footerView addSubview:self.insertCar];
+        [self.footerView addSubview:self.editCar];
+        self.tableView.tableFooterView  = self.footerView;
+        
+    }
+    
+    
+    
+    return self;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
